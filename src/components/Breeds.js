388e411eb6a6aabb701e70d1e21470d807458377
Vicky <kosenchiha@gmail.com>
2019-06-19 10:15:47 +0100

    Changes in Breeds.js allow the user to select any breed in the Select-a-Breed  panel by loading all the possible breeds from the API

--- src/components/Breeds.js
+++ src/components/Breeds.js
@@ -2,20 +2,34 @@ import React from "react";
 import "./Breeds.css";
 
 class Breeds extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { options: [] };
+  }
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => res.json())
+      .then((data) => {
+        let listOfbreeds = Object.keys(data.message);
+        this.setState({
+          options: listOfbreeds
+        });
+      });
+  }
   render() {
     return (
       <div className="Breeds">
         <h2 className="Breeds-title">Select a Breed</h2>
         <p>
-          <select className="Breeds-select">
-            <option>beagle</option>
-            <option>bluetick</option>
-            <option>bullterrier-staffordshire</option>
-            <option>malinois</option>
-            <option>wolfhound-irish</option>
+          <select className="Breeds-select" onChange={this.handleChange}>
+            {this.state.options.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
           </select>
         </p>
-        <img className="Breeds-image" src="http://via.placeholder.com/300x300" />
+        <img className="Breeds-image" alt="" src="http://via.placeholder.com/300x300" />
         <p>
           <button className="Breeds-button">Show me more!</button>
         </p>
